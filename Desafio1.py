a347fa9a6989f05109f57d2c848024ed23af7c15
Facundo <facundoperi01@gmail.com>
2022-03-25 17:38:59 -0300

    Primer Commit -- Agrego Desafio 1 --

---
+++ Desafio1.py
@@ -0,0 +1,10 @@
+# Queremos ingresar un número desde el teclado 
+# e imprimir si el número es o no par.
+
+num  = int(input("Ingrese un numero: "))
+
+if (num % 2 == 0):
+    print(" ES PAR ")
+else:
+    print(" NO ES PAR ")
+    
